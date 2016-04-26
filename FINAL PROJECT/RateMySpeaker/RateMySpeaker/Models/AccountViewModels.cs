f19ca88daa1dc647809fad1a0745c1224616d3a5
Paige <paige.fenwick@att.net>
2016-04-26 00:44:46 -0400

    Added fields to the registration page, updated the login page, updated the about page

--- FINAL PROJECT/RateMySpeaker/RateMySpeaker/Models/AccountViewModels.cs
+++ FINAL PROJECT/RateMySpeaker/RateMySpeaker/Models/AccountViewModels.cs
@@ -65,6 +65,15 @@ namespace RateMySpeaker.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+      
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
